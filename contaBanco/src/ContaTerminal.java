fde2446f322d3932fbc2f7cfc6897a14fe092e48
Dayane <dayanec.dacunha@gmmail.com>
2024-05-11 13:52:52 -0300

    ContaTerminal.java

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,6 +1,27 @@
-public class ContaTerminal {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class Main {
+
+    public static void main(String[] args) {
+        
+        double saldo = 25;
+        double valorSolicitado = 18;
+
+        if( valorSolicitado <= saldo){
+            saldo = saldo - valorSolicitado;
+           } else { 
+              System.out.println ("Saldo indisponivel  " + saldo);
+           }
+            System.out.println ("Saldo atual  " + saldo );
+    
+    
+     saldo = 15;
+     valorSolicitado = 22;
+
+    if( valorSolicitado <= saldo){
+        saldo = saldo - valorSolicitado;
+       } else { 
+          System.out.println ("Saldo indisponivel  " + saldo);
+       }
+        System.out.println ("Saldo atual  " + saldo );
+}
 } 
